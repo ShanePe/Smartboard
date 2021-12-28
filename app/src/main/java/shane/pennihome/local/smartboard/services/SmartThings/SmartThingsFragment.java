d83ff214f8470d43c749e1a22bb413ecf9dfe799
Shane Pennicott <Shane.Pennicott@gmail.com>
2021-12-28 19:49:30 +0000

    Moved Hue to V2 API

--- app/src/main/java/shane/pennihome/local/smartboard/services/SmartThings/SmartThingsFragment.java
+++ app/src/main/java/shane/pennihome/local/smartboard/services/SmartThings/SmartThingsFragment.java
@@ -2,7 +2,9 @@ package shane.pennihome.local.smartboard.services.SmartThings;
 
 import android.annotation.SuppressLint;
 import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.net.Uri;
+import android.net.http.SslError;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
@@ -10,6 +12,10 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.webkit.SslErrorHandler;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
+import android.webkit.WebResourceResponse;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
@@ -31,12 +37,20 @@ public class SmartThingsFragment extends IRegisterServiceFragment {
         View view = inflater.inflate(R.layout.fragment_smart_things, container, false);
 
         WebView web = view.findViewById(R.id.st_webview);
+        web.setWebViewClient(new WebViewClient());
+
         web.getSettings().setJavaScriptEnabled(true);
         web.getSettings().setAllowContentAccess(true);
         web.getSettings().setAppCacheEnabled(true);
         web.getSettings().setGeolocationEnabled(true);
         web.getSettings().setDomStorageEnabled(true);
         web.getSettings().setDatabaseEnabled(true);
+
+        web.getSettings().setLoadWithOverviewMode(true);
+        web.getSettings().setUseWideViewPort(true);
+        web.getSettings().setSupportZoom(false);
+        web.setWebContentsDebuggingEnabled(true);
+        web.setBackgroundColor(Color.TRANSPARENT);
 
         String requestUrl = (SmartThingsService.ST_OAUTH_URL +
                 "?redirect_uri=" + SmartThingsService.ST_REDIRECT_URI +
@@ -45,21 +59,38 @@ public class SmartThingsFragment extends IRegisterServiceFragment {
 
         getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
         final ProgressDialog dialog = new ProgressDialog();
+
         dialog.setMessage("Communicating with SmartThings");
+
         web.setWebViewClient(new WebViewClient() {
             boolean authComplete = false;
 
             @Override
             public void onPageStarted(WebView view, String url, Bitmap favicon) {
-                dialog.show(getContext());
-                view.setVisibility(View.GONE);
+                //dialog.show(getContext());
+                //view.setVisibility(View.GONE);
                 super.onPageStarted(view, url, favicon);
+            }
+
+            @Override
+            public void onReceivedError(WebView view, WebResourceRequest request, WebResourceError error) {
+                super.onReceivedError(view, request, error);
+            }
+
+            @Override
+            public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
+                super.onReceivedSslError(view, handler, error);
+            }
+
+            @Override
+            public void onReceivedHttpError(WebView view, WebResourceRequest request, WebResourceResponse errorResponse) {
+                super.onReceivedHttpError(view, request, errorResponse);
             }
 
             @Override
             public void onPageFinished(WebView view, String url) {
                 try {
-                    dialog.dismiss();
+                  //  dialog.dismiss();
                     super.onPageFinished(view, url);
                     view.setVisibility(View.VISIBLE);
                     if (url.contains("?code=") && !authComplete) {
@@ -72,6 +103,7 @@ public class SmartThingsFragment extends IRegisterServiceFragment {
 
                     } else if (url.contains("error=access_denied"))
                         throw new Exception("Access denied");
+
                 } catch (Exception ex) {
                     Toast.makeText(getActivity(), "Error : " + ex.getMessage(), Toast.LENGTH_SHORT).show();
                     authComplete = true;
